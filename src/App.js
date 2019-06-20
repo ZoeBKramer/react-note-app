7370190d6aac399e0cdffcfce0267bec23bb7b59
ZoeBKramer <zoebkramer@gmail.com>
2019-06-20 15:15:29 -0700

    add note and list components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/nav.js';
+import List from './components/list.js';
+import Note from './components/note.js';
 
 class App extends Component {
 render() {
   return (
     <div className="App">
       <Nav />
+      <List />
+      <Note />
     </div>
   );
 }
